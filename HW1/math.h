684c508f0aae134249b3019fb884d0a444a82693
Oleksii Stepanyk <o.stepanik06@gmail.com>
2026-03-01 23:53:38 +0200

    Function definition for math and comparison

--- HW1/math.h
+++ HW1/math.h
@@ -1,4 +1,16 @@
 #ifndef MATH_H
 #define MATH_H
 
+float abs_f(float number);
+double abs_d(double number);
+
+float min_f(float a, float b);
+double min_d(double a, double b);
+
+float max_f(float a, float b);
+double max_d(double a, double b);
+
+float clamp_f(float number, float min, float max);
+double clamp_d(double number, double min, double max);
+
 #endif // MATH_H
