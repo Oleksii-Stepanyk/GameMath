d2540d391c3ea945f7342f71b8d12ac168f58190
Oleksii Stepanyk <o.stepanik06@gmail.com>
2026-03-05 09:04:25 +0200

    Removed magic numbers

--- HW1/src/checks.c
+++ HW1/src/checks.c
@@ -1,6 +1,14 @@
 #include "checks.h"
 #include "representation.h"
 
+static const uint32_t F_SIGN_MASK = 0x7FFFFFFF;
+static const uint32_t F_EXP_MASK  = 0x7F800000;
+static const uint32_t F_MANT_MASK = 0x007FFFFF;
+
+static const uint64_t D_SIGN_MASK = 0x7FFFFFFFFFFFFFFF;
+static const uint64_t D_EXP_MASK  = 0x7FF0000000000000;
+static const uint64_t D_MANT_MASK = 0x000FFFFFFFFFFFFF;
+
 bool is_signed_f(float number)
 {
     uint32_t bits = f_to_bits(number);
@@ -16,13 +24,13 @@ bool is_signed_d(double number)
 bool is_zero_f(float number)
 {
     uint32_t bits = f_to_bits(number);
-    return (bits & 0x7FFFFFFF) == 0;
+    return (bits & F_SIGN_MASK) == 0;
 }
 
 bool is_zero_d(double number)
 {
     uint64_t bits = d_to_bits(number);
-    return (bits & 0x7FFFFFFFFFFFFFFF) == 0;
+    return (bits & D_SIGN_MASK) == 0;
 }
 
 bool is_pos_zero_f(float number)
@@ -48,13 +56,13 @@ bool is_neg_zero_d(double number)
 bool is_inf_f(float number)
 {
     uint32_t bits = f_to_bits(number);
-    return (bits & 0x7FFFFFFF) == 0x7F800000;
+    return (bits & F_SIGN_MASK) == F_EXP_MASK;
 }
 
 bool is_inf_d(double number)
 {
     uint64_t bits = d_to_bits(number);
-    return (bits & 0x7FFFFFFFFFFFFFFF) == 0x7FF0000000000000;
+    return (bits & D_SIGN_MASK) == D_EXP_MASK;
 }
 
 bool is_pos_inf_f(float number)
@@ -80,49 +88,49 @@ bool is_neg_inf_d(double number)
 bool is_nan_f(float number)
 {
     uint32_t bits = f_to_bits(number);
-    return (bits & 0x7FFFFFFF) > 0x7F800000;
+    return (bits & F_SIGN_MASK) > F_EXP_MASK;
 }
 
 bool is_nan_d(double number)
 {
     uint64_t bits = d_to_bits(number);
-    return (bits & 0x7FFFFFFFFFFFFFFF) > 0x7FF0000000000000;
+    return (bits & D_SIGN_MASK) > D_EXP_MASK;
 }
 
 bool is_finite_f(float number)
 {
     uint32_t bits = f_to_bits(number);
-    return (bits & 0x7F800000) != 0x7F800000;
+    return (bits & F_EXP_MASK) != F_EXP_MASK;
 }
 
 bool is_finite_d(double number)
 {
     uint64_t bits = d_to_bits(number);
-    return (bits & 0x7FF0000000000000) != 0x7FF0000000000000;
+    return (bits & D_EXP_MASK) != D_EXP_MASK;
 }
 
 bool is_subnormal_f(float number)
 {
     uint32_t bits = f_to_bits(number);
-    return (bits & 0x7F800000) == 0 && (bits & 0x007FFFFF) != 0;
+    return (bits & F_EXP_MASK) == 0 && (bits & F_MANT_MASK) != 0;
 }
 
 bool is_subnormal_d(double number)
 {
     uint64_t bits = d_to_bits(number);
-    return (bits & 0x7FF0000000000000) == 0 && (bits & 0x000FFFFFFFFFFFFF) != 0;
+    return (bits & D_EXP_MASK) == 0 && (bits & D_MANT_MASK) != 0;
 }
 
 bool is_normal_f(float number)
 {
     uint32_t bits = f_to_bits(number);
-    uint32_t exp = bits & 0x7F800000;
-    return exp != 0 && exp != 0x7F800000;
+    uint32_t exp = bits & F_EXP_MASK;
+    return exp != 0 && exp != F_EXP_MASK;
 }
 
 bool is_normal_d(double number)
 {
     uint64_t bits = d_to_bits(number);
-    uint64_t exp = bits & 0x7FF0000000000000;
-    return exp != 0 && exp != 0x7FF0000000000000;
+    uint64_t exp = bits & D_EXP_MASK;
+    return exp != 0 && exp != D_EXP_MASK;
 }
